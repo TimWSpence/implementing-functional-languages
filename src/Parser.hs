b66f588aa04157d20261f3928c8b234fe66a222e
Tim Spence <timothywspence@gmail.com>
2019-10-17 10:40:05 +0100

    Add line numbers to lexer

--- src/Parser.hs
+++ src/Parser.hs
@@ -5,22 +5,27 @@ module Parser
 import           Data.Char
 
 
-type Token = String
+type LineNumber = Int
+type Token = (LineNumber, String)
 
 clex :: String -> [Token]
-clex [] = []
-clex (x:y:t) | [x,y] `elem` twoCharOps = [x,y] : clex t
-clex (c:cs)
-  | isSpace c = clex cs
-  | isDigit c = num_token : clex num_rest
-  | isAlpha c = var_tok : clex var_rest
+clex = _clex 0
   where
-    num_token = c : takeWhile isDigit cs
-    num_rest = dropWhile isDigit cs
-    var_tok = c : takeWhile isIdChar cs
-    var_rest = dropWhile isIdChar cs
+  _clex :: Int -> String -> [Token]
+  _clex _ [] = []
+  _clex n (x:y:t) | [x,y] `elem` twoCharOps = (n, [x,y]) : _clex n t
+  _clex n (c:cs)
+    | c == '\n' = _clex (n+1) cs
+    | isSpace c = _clex n cs
+    | isDigit c = (n, num_token) : _clex n num_rest
+    | isAlpha c = (n, var_tok) : _clex n var_rest
+    where
+      num_token = c : takeWhile isDigit cs
+      num_rest = dropWhile isDigit cs
+      var_tok = c : takeWhile isIdChar cs
+      var_rest = dropWhile isIdChar cs
 
-    isIdChar c = isAlpha c || isDigit c || (c == '_')
+      isIdChar c = isAlpha c || isDigit c || (c == '_')
 
 twoCharOps :: [String]
 twoCharOps = ["==", "~=", ">=", "<=", "->"]
