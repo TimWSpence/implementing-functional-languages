1286402fef960205eabeff2523265f3e584fee32
Tim Spence <timothywspence@gmail.com>
2019-10-28 15:24:37 +0000

    Introduce functor/applicative/alternative instances for parser
    - Make things nicer and more idiomatic Haskell

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,6 +4,7 @@ module Parser
   ) where
 
 import           AST
+import           Control.Applicative
 import           Parser.CombinatorParser
 import           Parser.Lexer
 
@@ -11,7 +12,7 @@ parse :: String -> CoreProgram
 parse = syntax . clex
 
 syntax :: [Token] -> CoreProgram
-syntax = take_first_parse . pProgram
+syntax = take_first_parse . runParser pProgram
   where
     take_first_parse ((prog, []):_) = prog
     take_first_parse (_: others)    = take_first_parse others
@@ -20,76 +21,79 @@ syntax = take_first_parse . pProgram
 pProgram :: Parser CoreProgram
 pProgram = pOneOrMoreWithSep pSc (pLit ";")
 
+liftA4 :: Applicative a => (v -> w -> x -> y -> z) -> a v -> a w -> a x -> a y -> a z
+liftA4 f v w x y = f <$> v <*> w <*> x <*> y
+
 pSc :: Parser CoreScDefn
-pSc = pThen4 mk_sc pVar (pZeroOrMore pVar) (pLit "=") pExpr
+pSc = liftA4 mk_sc pVar (pZeroOrMore pVar) (pLit "=") pExpr
   where
     mk_sc name vars _ expr = (name, vars, expr)
 
 --TODO separate parsing of atomic expressions eg should only be able to apply to an atomic expr
 pExpr :: Parser CoreExpr
-pExpr = pELet `pAlt` pECase `pAlt` pELam `pAlt` pExpr1
+pExpr = pELet <|> pECase <|> pELam <|> pExpr1
   where
-    pELet = pThen4 (\_ binds _ expr -> ELet NotRecursive binds expr) (pLit "let") (pOneOrMoreWithSep pLet (pLit ";")) (pLit "in") pExpr
-      `pAlt` pThen4 (\_ binds _ expr -> ELet Recursive binds expr) (pLit "letrec") (pOneOrMoreWithSep pLet (pLit ";")) (pLit "in") pExpr
+    pELet = liftA4 (\_ binds _ expr -> ELet NotRecursive binds expr) (pLit "let") (pOneOrMoreWithSep pLet (pLit ";")) (pLit "in") pExpr
+      <|> liftA4 (\_ binds _ expr -> ELet Recursive binds expr) (pLit "letrec") (pOneOrMoreWithSep pLet (pLit ";")) (pLit "in") pExpr
       where
-        pLet = pThen3 (\var _ e -> (var, e)) pVar (pLit "=") pExpr
+        pLet = liftA3 (\var _ e -> (var, e)) pVar (pLit "=") pExpr
 
-    pECase = pThen4 (\_ e _ cases -> ECase e cases) (pLit "case") pExpr (pLit "of") (pOneOrMoreWithSep pCase (pLit ";"))
+    pECase = liftA4 (\_ e _ cases -> ECase e cases) (pLit "case") pExpr (pLit "of") (pOneOrMoreWithSep pCase (pLit ";"))
       where
-        pTag = pThen3 (\_ n _ -> n) (pLit "<") pNum (pLit ">")
-        pCase = pThen4 (\t as _ e -> (t, as, e)) pTag (pZeroOrMore pVar) (pLit "->") pExpr
+        pTag = liftA3 (\_ n _ -> n) (pLit "<") pNum (pLit ">")
+        pCase = liftA4 (\t as _ e -> (t, as, e)) pTag (pZeroOrMore pVar) (pLit "->") pExpr
 
-    pELam = pThen4 (\_ vars _ e -> ELam vars e) (pLit "\\") (pOneOrMore pVar) (pLit ".") pExpr
+    pELam = liftA4 (\_ vars _ e -> ELam vars e) (pLit "\\") (pOneOrMore pVar) (pLit ".") pExpr
 
 pExpr1 :: Parser CoreExpr
-pExpr1 = pOr `pAlt` pExpr2
+pExpr1 = pOr <|> pExpr2
   where
-    pOr = pThen3 (\e _ e2 -> EBin (BOp Or) e e2) pExpr2 (pLit "||") pExpr1
+    pOr = liftA3 (\e _ e2 -> EBin (BOp Or) e e2) pExpr2 (pLit "||") pExpr1
 
 pExpr2 :: Parser CoreExpr
-pExpr2 = pAnd `pAlt` pExpr3
+pExpr2 = pAnd <|> pExpr3
   where
-    pAnd = pThen3 (\e _ e2 -> EBin (BOp And) e e2) pExpr3 (pLit "&&") pExpr2
+    pAnd = liftA3 (\e _ e2 -> EBin (BOp And) e e2) pExpr3 (pLit "&&") pExpr2
 
 pExpr3 :: Parser CoreExpr
-pExpr3 = pRel `pAlt` pExpr4
+pExpr3 = pRel <|> pExpr4
   where
-    pRel = pLT `pAlt` pLE `pAlt` pEQ `pAlt` pNEQ `pAlt` pGE `pAlt` pGT
-    pLT = pThen3 (\e _ e2 -> EBin (ROp RLT) e e2) pExpr4 (pLit "<") pExpr3 --TODO finish this mapping
-    pLE = pThen3 (\e _ e2 -> EBin (ROp RLE) e e2) pExpr4 (pLit "<=") pExpr3 --TODO finish this mapping
-    pEQ = pThen3 (\e _ e2 -> EBin (ROp REQ) e e2) pExpr4 (pLit "==") pExpr3 --TODO finish this mapping
-    pNEQ = pThen3 (\e _ e2 -> EBin (ROp RNEQ) e e2) pExpr4 (pLit "~=") pExpr3 --TODO finish this mapping
-    pGE = pThen3 (\e _ e2 -> EBin (ROp RGE) e e2) pExpr4 (pLit ">=") pExpr3 --TODO finish this mapping
-    pGT = pThen3 (\e _ e2 -> EBin (ROp RGT) e e2) pExpr4 (pLit ">") pExpr3 --TODO finish this mapping
+    pRel = pLT <|> pLE <|> pEQ <|> pNEQ <|> pGE <|> pGT
+    pLT = liftA3 (\e _ e2 -> EBin (ROp RLT) e e2) pExpr4 (pLit "<") pExpr3 --TODO finish this mapping
+    pLE = liftA3 (\e _ e2 -> EBin (ROp RLE) e e2) pExpr4 (pLit "<=") pExpr3 --TODO finish this mapping
+    pEQ = liftA3 (\e _ e2 -> EBin (ROp REQ) e e2) pExpr4 (pLit "==") pExpr3 --TODO finish this mapping
+    pNEQ = liftA3 (\e _ e2 -> EBin (ROp RNEQ) e e2) pExpr4 (pLit "~=") pExpr3 --TODO finish this mapping
+    pGE = liftA3 (\e _ e2 -> EBin (ROp RGE) e e2) pExpr4 (pLit ">=") pExpr3 --TODO finish this mapping
+    pGT = liftA3 (\e _ e2 -> EBin (ROp RGT) e e2) pExpr4 (pLit ">") pExpr3 --TODO finish this mapping
 
 pExpr4 :: Parser CoreExpr
-pExpr4 = pPlus `pAlt` pMinus `pAlt` pExpr5
+pExpr4 = pPlus <|> pMinus <|> pExpr5
   where
-    pPlus = pThen3 (\e _ e2 -> EBin (NOp Plus) e e2) pExpr5 (pLit "+") pExpr4
-    pMinus = pThen3 (\e _ e2 -> EBin (NOp Minus) e e2) pExpr5(pLit "-") pExpr4
+    pPlus = liftA3 (\e _ e2 -> EBin (NOp Plus) e e2) pExpr5 (pLit "+") pExpr4
+    pMinus = liftA3 (\e _ e2 -> EBin (NOp Minus) e e2) pExpr5(pLit "-") pExpr4
 
 
 pExpr5 :: Parser CoreExpr
-pExpr5 = pTimes `pAlt` pDiv `pAlt` pExpr6
+pExpr5 = pTimes <|> pDiv <|> pExpr6
   where
-    pTimes = pThen3 (\e _ e2 -> EBin (NOp Times) e e2) pExpr6 (pLit "*") pExpr5
-    pDiv = pThen3 (\e _ e2 -> EBin (NOp Div) e e2) pExpr6 (pLit "/") pExpr5
+    pTimes = liftA3 (\e _ e2 -> EBin (NOp Times) e e2) pExpr6 (pLit "*") pExpr5
+    pDiv = liftA3 (\e _ e2 -> EBin (NOp Div) e e2) pExpr6 (pLit "/") pExpr5
 
 pExpr6 :: Parser CoreExpr
-pExpr6 = pAExpr `pAlt` pEAp
+pExpr6 = pAExpr <|> pEAp
   where
-    pAExpr = pEVar `pAlt` pENum `pAlt` pEConstr `pAlt` (pParens pExpr)
+    pAExpr = pEVar <|> pENum <|> pEConstr <|> (pParens pExpr)
 
-    pEVar = pApply pVar EVar
+    pEVar = fmap EVar pVar
 
-    pENum = pApply pNum ENum
+    pENum = fmap ENum pNum
 
-    pEConstr = pThen4 (\_ _ (c, arity) _ -> EConstr c arity) (pLit "Pack") (pLit "{") pTags (pLit "}")
+    pEConstr = liftA4 (\_ _ (c, arity) _ -> EConstr c arity) (pLit "Pack") (pLit "{") pTags (pLit "}")
       where
-        pTags = pThen3 (\c _ arity -> (c, arity)) pNum (pLit ",") pNum
+        pTags = liftA3 (\c _ arity -> (c, arity)) pNum (pLit ",") pNum
 
     --A bit weird due to left recursion in the "obvious" implementation
-    pEAp = pApply (pOneOrMore pAExpr) apChain
+    pEAp = fmap apChain (pOneOrMore pAExpr)
       where
         apChain [fn,arg] = EAp fn arg
         apChain _        = error "Syntax error in apply"
--- src/parser/CombinatorParser.hs
+++ src/parser/CombinatorParser.hs
@@ -1,19 +1,12 @@
 module Parser.CombinatorParser
-  ( Parser
-  , pFail
+  ( Parser(..)
   , pParens
   , pLit
   , pSat
   , pVar
   , pNum
-  , pAlt
-  , pThen
-  , pThen3
-  , pThen4
   , pZeroOrMore
   , pOneOrMore
-  , pEmpty
-  , pApply
   , pOneOrMoreWithSep
   ) where
 
@@ -21,7 +14,28 @@ import           Data.Char
 
 import           Parser.Lexer
 
-type Parser a = [Token] -> [(a, [Token])]
+import           Control.Applicative
+
+newtype Parser a = Parser { runParser :: [Token] -> [(a, [Token])] }
+
+instance Functor Parser where
+  fmap f p = Parser $ \toks -> do
+    (v, toks2) <- runParser p toks
+    return (f v, toks2)
+
+instance Applicative Parser where
+  pure a = Parser $ \toks -> [(a, toks)]
+
+  f <*> p = Parser $ \toks -> do
+    (fn, toks2) <- runParser f toks
+    (v, toks3)  <- runParser p toks2
+    return (fn v, toks3)
+
+instance Alternative Parser where
+
+  empty = Parser $ \_ -> []
+
+  p1 <|> p2 = Parser $ \toks -> runParser p1 toks ++ runParser p2 toks
 
 pLit :: String -> Parser String
 pLit s = pSat (== s)
@@ -32,63 +46,30 @@ pVar = pSat $ \s -> s `notElem` keywords && isVar s
     isVar []     = False
     isVar (c:cs) = isAlpha c && all isIdChar cs
 
-pAlt :: Parser a -> Parser a -> Parser a
-pAlt p1 p2 toks = p1 toks ++ p2 toks
-
--- TODO applicative instance for Parser
-pThen :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
-pThen f p1 p2 toks = do
-  (a, toks1) <- p1 toks
-  (b, toks2) <- p2 toks1
-  return (f a b, toks2)
-
-pThen3 :: (a -> b -> c -> d) -> Parser a -> Parser b -> Parser c -> Parser d
-pThen3 f p1 p2 p3 toks = do
-  (f2, toks1) <- pThen f p1 p2 toks
-  (d, toks2) <- p3 toks1
-  return (f2 d, toks2)
-
-pThen4 :: (a -> b -> c -> d -> e) -> Parser a -> Parser b -> Parser c -> Parser d -> Parser e
-pThen4 f p1 p2 p3 p4 toks = do
-  (f2, toks1) <- pThen3 f p1 p2 p3 toks
-  (e, toks2) <- p4 toks1
-  return (f2 e, toks2)
-
 pZeroOrMore :: Parser a -> Parser [a]
-pZeroOrMore p = pOneOrMore p `pAlt` pEmpty []
+pZeroOrMore p = pOneOrMore p <|> pure []
 
 pOneOrMore :: Parser a -> Parser [a]
-pOneOrMore p toks = do
-  (a, toks1) <- p toks
-  (rest, toks2) <- pZeroOrMore p toks1
+pOneOrMore p = Parser $ \toks -> do
+  (a, toks1) <- runParser p toks
+  (rest, toks2) <- runParser (pZeroOrMore p) toks1
   return (a:rest, toks2)
-
-pEmpty :: a -> Parser a
-pEmpty a toks = [(a, toks)]
-
--- TODO functor instance for Parser
-pApply :: Parser a -> (a -> b) -> Parser b
-pApply p f toks = do
-  (a, toks1) <- p toks
-  return (f a, toks1)
 
 pOneOrMoreWithSep :: Parser a -> Parser b -> Parser [a]
-pOneOrMoreWithSep pa pb toks = do
-  (a, toks1) <- pa toks
-  (rest, toks2) <- pab toks1
+pOneOrMoreWithSep pa pb = Parser $ \toks -> do
+  (a, toks1) <- runParser pa toks
+  (rest, toks2) <- runParser pab toks1
   return (a:rest, toks2)
     where
-      pab = pZeroOrMore (pThen (\_ a -> a) pb pa)
+      pab = pZeroOrMore (liftA2 (\_ a -> a) pb pa)
 
 pSat :: (String -> Bool) -> Parser String
-pSat pred ((_, tok):toks) | pred tok = [(tok, toks)]
-pSat _ _                  = []
+pSat pred = Parser $ \t -> case t of
+  ((_, tok):toks) | pred tok -> [(tok, toks)]
+  _                          -> []
 
 pNum :: Parser Int
-pNum = pApply (pSat (all isDigit)) read
+pNum = read <$> (pSat (all isDigit))
 
 pParens :: Parser a -> Parser a
-pParens p = pThen3 (\_ v _ -> v) (pLit "(") p (pLit ")")
-
-pFail :: Parser a
-pFail _ = []
+pParens p = liftA3 (\_ v _ -> v) (pLit "(") p (pLit ")")
